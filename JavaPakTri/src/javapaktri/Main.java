d6d6a8cd2486f2b5d1b0b6d5cce76ca6d819b52e
Geary24 <gearyeramusephraim@gmail.com>
2026-01-12 19:01:15 +0700

    Add files via upload

---
+++ JavaPakTri/src/javapaktri/Main.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javapaktri;
+
+/**
+ *
+ * @author user
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        // Nulis kode disini
+        int Angka;
+        Angka=10;
+        
+        System.out.println (Angka);
+        System.out.println("Angka yang anda inputkan: "+Angka);
+        Angka=100;
+        System.out.println("Angka Terabaru adalah: "+Angka);
+        
+        double desimal=10.355;
+        float pecahan=13.4555f;
+        System.out.println(desimal);
+        System.out.println("Angka pecahan yang di input");
+        
+        String nama="Geary";
+        char abjad='A';
+        System.out.println(nama);
+        System.out.println(abjad);
+        
+        boolean benar= true;
+        boolean salah= false;
+        System.out.println(benar);
+        System.out.println(salah);
+        
+        desimal=100.234;
+        System.out.println(desimal);
+    }
+    
+}
